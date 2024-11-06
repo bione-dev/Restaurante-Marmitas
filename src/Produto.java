b953e0180d66dcc58d30125dde1ad58bb30a7eaa
Gustavo Bione <bione.dev@gmail.com>
2024-11-06 00:13:44 -0300

    Cadastro dos produtos fornecidos Postgres

---
+++ src/Produto.java
@@ -0,0 +1,39 @@
+public class Produto {
+    private int id;
+    private String nome;
+    private double preco;
+    private String tipo; // Marmita Completa, Marmita+Refri, Marmita+Suco
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
